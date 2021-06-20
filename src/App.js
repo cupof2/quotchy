4f5be0cf045f6035dd2af3908546c4608e19ec28
rjworks <arjayandal05@yahoo.com>
2021-06-19 23:13:25 -0600

    Finish

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
 import './App.css';
+import {bgColors, bgFontColors} from './colors';
+import {useEffect, useRef} from 'react';
+
+const Quote = require('inspirational-quotes');
+const quotes = [{}];
+
+let j = 0;
+let c = 0;
+for(let i = 0; c < 200; i++) {
+    const quote = Quote.getRandomQuote();
+    if(quote.length <= 50) {
+        // console.log(quote + ": " + quote.length)
+        if(j > bgColors.length)
+            j = 1;
+
+        quotes[c] = {
+            "quote": quote,
+            "bgColor": bgColors[j],
+            "fontColor": bgFontColors[bgColors[j]]
+        }
+        c++;
+        j++;
+    }
+}
 
 function App() {
-  return (
-    <div>
-    </div>
-  );
+    const ref = useRef(null);
+
+    useEffect(() => {
+        window.scrollTo({behavior: 'smooth', top: ref.current.offsetTop})
+    }, [])
+
+    return (
+        <div className="container" ref={ref}>
+            {/*<section ref={ref}>*/}
+            {quotes.map((el, idx) => {
+                return <div
+                    className="card"
+                    id={"page-" + idx}
+                    key={idx}
+                    style={{
+                        backgroundColor: `${quotes[idx].bgColor}`,
+                        color: `${quotes[idx].fontColor}`
+                    }}
+                >
+                    {quotes[idx].quote}
+                </div>
+            })}
+            {/*</section>*/}
+        </div>
+    );
 }
+
 
 export default App;
---
+++ src/colors.js
@@ -0,0 +1,16 @@
+const bgFontColors = {
+    "#FFFFFF": "black",
+    "#F038FF": "white",
+    "#FFEE56": "black",
+    "#54FF9F": "black",
+    "#427AFF": "white",
+}
+const bgColors = [
+    "#FFFFFF",
+    "#F038FF",
+    "#FFEE56",
+    "#54FF9F",
+    "#427AFF",
+]
+
+export {bgColors, bgFontColors}
