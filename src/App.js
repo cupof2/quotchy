1b7d5d52c2a9d545bbb93f5e439fe5b6652c0249
rjworks <arjayandal05@yahoo.com>
2021-06-22 17:30:09 -0600

    Welp

--- src/App.js
+++ src/App.js
@@ -1,56 +1,113 @@
 import './App.css';
 import {bgColors, bgFontColors} from './colors';
-import {useEffect, useRef} from 'react';
+import {useEffect, useRef, useState} from 'react';
 
 const Quote = require('inspirational-quotes');
 const quotes = [{}];
 
 let j = 0;
 let c = 0;
-for(let i = 0; c < 200; i++) {
+const addedQuotes = [];
+
+for(let i = 0; i < 200; i++) {
     const quote = Quote.getRandomQuote();
     if(quote.length <= 50) {
-        // console.log(quote + ": " + quote.length)
         if(j > bgColors.length)
             j = 1;
 
-        quotes[c] = {
-            "quote": quote,
-            "bgColor": bgColors[j],
-            "fontColor": bgFontColors[bgColors[j]]
+        if(!addedQuotes.includes(quote)) {
+            quotes[c] = {
+                "quote": quote,
+                "bgColor": bgColors[j],
+                "fontColor": bgFontColors[bgColors[j]]
+            }
+            addedQuotes.push(quote);
+            c++;
+            j++;
         }
-        c++;
-        j++;
     }
 }
 
-function App() {
-    const ref = useRef(null);
+let totalQuotes = quotes.length;
+let coolDown = new Date().getTime();
+function Quotchy() {
+    const [nextPage, setNextPage] = useState(1);
+    const scrollRef = useRef([]);
+
+    const handleWheel = (e) => {
+        if(e.deltaY > 0) {
+            const d = new Date();
+            if(coolDown > d.getTime()) {
+                return;
+            }
+            coolDown = d.getTime() + (800);
+            if(nextPage > totalQuotes) {
+                window.location.reload();
+            } else {
+                window.scrollTo({
+                    top: scrollRef.current[nextPage].offsetTop,
+                    behavior: "smooth"
+                });
+                setNextPage(nextPage + 1);
+            }
+        } else if(e.deltaY < 0) {
+            alert("Never look back soN!");
+        }
+    }
 
     useEffect(() => {
-        window.scrollTo({behavior: 'smooth', top: ref.current.offsetTop})
+        window.scrollTo({top: -1, behavior: 'smooth'});
+        // const scrollToCurrentPage = () => {
+        // if(nextPage > 2){
+        //     window.scrollTo({
+        //         top: scrollRef.current[nextPage].offsetTop,
+        //         behavior: "smooth"
+        //     });
+        //     console.log("scroll to " + nextPage)
+        // }
+        // console.log("I've been resized")
+        // }
+
+        // window.addEventListener('resize', scrollToCurrentPage);
+        // scrollToCurrentPage();
+        // return () => window.removeEventListener('resize', scrollToCurrentPage);
     }, [])
 
+
     return (
-        <div className="container" ref={ref}>
-            {/*<section ref={ref}>*/}
+        <div onWheel={e => handleWheel(e)} className="quotchy-container">
             {quotes.map((el, idx) => {
                 return <div
-                    className="card"
-                    id={"page-" + idx}
+                    className="quotchy-card"
+                    ref={el => scrollRef.current[idx] = el}
+                    id={"quotchy-page-" + (idx)}
                     key={idx}
                     style={{
                         backgroundColor: `${quotes[idx].bgColor}`,
-                        color: `${quotes[idx].fontColor}`
+                        color: `${quotes[idx].fontColor}`,
                     }}
                 >
                     {quotes[idx].quote}
                 </div>
             })}
-            {/*</section>*/}
+            <div
+                style={
+                    {
+                        fontStyle: "normal",
+                        backgroundColor: '#ffd6a5',
+                        color: 'black',
+                        paddingTop: 0,
+                        paddingBottom: 0
+                    }
+                }
+                className="quotchy-card"
+                id={"quotchy-page-" + totalQuotes}
+                ref={el => scrollRef.current[totalQuotes] = el}>
+                Scroll down for a new set of quotes :-)
+            </div>
         </div>
     );
 }
 
 
-export default App;
+export default Quotchy;
